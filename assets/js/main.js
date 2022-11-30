b2e579197051d81af2cf774c8b2b14f4791431ff
Jhonatan Siqueira Pereira <79463243+JhonatanSiqueira@users.noreply.github.com>
2022-11-30 14:41:31 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+
+
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 16;
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+function listPokemonByName(){
+    var inputPokemon = document.getElementById("search");
+    const pokemons = pokeApi.getPokemonDetail;
+    console.log("You are searching for: " + inputPokemon.value);
+
+    if (inputPokemon.value == pokemons) {
+        console.log("existe")
+    } else {
+        console.log("existe não")
+    }
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+ 
+})
